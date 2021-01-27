b94d99634ae6a243bd2126bb345d2b621af74942
alejandro medici <el.aleyo@gmail.com>
2021-01-26 21:23:37 -0300

    Added sessionStorage sample

--- storage_001.js
+++ storage_001.js
@@ -1,3 +1,12 @@
-//localStorage.setItem("Nombre", "Ale");
+localStorage.setItem("Nombre", "Ale");
 var valor1 = localStorage.Nombre;
 console.log(valor1);
+
+sessionStorage.setItem("Fecha", "26/01/2021");
+
+var valor2 = sessionStorage.getItem("Fecha");
+console.log(valor2);
+
+localStorage.clear();
+
+sessionStorage.removeItem("Fecha");
